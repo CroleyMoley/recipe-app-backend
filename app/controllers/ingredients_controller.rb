798118038b0afc4eac09d0935a5c20a2ebc7f18d
MisterAnthropy <tjcroley@outlook.com>
2020-12-22 15:45:27 -0800

    built controller actions for recipes and ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,19 @@
 class IngredientsController < ApplicationController
+    
+    def index
+        render json: Ingredient.all
+    end
+
+    def create
+        ingredient = Ingredient.new(ingredient_params)
+        if ingredient.save
+            render json: ingredient
+        end
+    end
+
+    private
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
+    end
+    
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,22 @@
 class RecipesController < ApplicationController
+
+    def index
+        render json: Recipe.all
+    end
+
+    
+    def create
+        recipe = Recipe.new(recipe_params)
+        if recipe.save
+            render json: recipe
+        end
+
+    end
+
+    private 
+    def recipe_params
+        params.require(:recipe).permit(:title)
+    end
+
+
 end
